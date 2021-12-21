e30021ebfdc2cf80fc21d72cd17d49e9bf2a8860
Mike Eng <mike.eng@adhocteam.us>
2021-12-21 12:24:05 -0500

    questions structure initially built

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
-$(document).foundation()
+$(document).foundation();
+
+$( document ).ready(function() {
+
+    // define initial variables
+    const quizContainer = $('#quiz');
+    const resultsContainer = $('#results');
+    const submitButton = $('#submit');
+    const myQuestions =[
+      {
+        question:"",
+        answers:{
+          a: "",
+          b: "",
+          c: "",
+          d: ""
+        },
+        correctAnswer: "c"
+      }
+    ]
+
+    function buildQuiz(){
+
+    }
+
+    function showResults(){
+
+    }
+
+    //display quiz right away
+    buildQuiz();
+
+    //on submit, show results
+    submitButton.on('click', showResults);
+});
